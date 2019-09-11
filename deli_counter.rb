47006ec1c37fed2e1f58636fd4c9e103f001a692
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 18:53:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(customers) 
+  if customers.length == 0
+    puts "The line is currently empty." 
+  else 
+    customers.each_with_index do | name, i |
+      katz_deli[i] = "#{i+1}. #{name}"
+    end
+  puts "The line is currently: #{katz_deli.join(" ")}"
+  end
+end
+
+def take_a_number(list, name)
+  list.push(name)
+  puts "Welcome, #{name}. You are number #{list.index(name) + 1} in line."
+end
+
+def now_serving(list)
+  if list.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+  puts "Currently serving #{list.first}."
+  list.shift
+  end 
+end
