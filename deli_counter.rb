00ce8ef0d28b1ec2615b77ff59eedc68d54173eb
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-11 20:48:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  end
+  array.each_with_index.map
+end
