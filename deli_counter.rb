a393a66cd544050f733d6a6bff908c94c1aa3533
Randel Jiao <randeljiao@gmail.com>
2019-09-14 17:51:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+ 
+end
+
+def line(space = 0)
+  space += 1
+  if space = 0
+    puts "The line is currently empty."
+end
+
+def now_serving(katz_deli, name)
+  puts "Currently serving #{name}."
+  if name <
+    puts "There is nobody waiting to be served!"
+end
